116177cf94c1c2b6dff7f872b2f468a9f14cb4a3
Srikrishnan K L <srikrishnan1947@gmail.com>
2020-10-19 21:55:19 +0530

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -14,6 +14,7 @@ const mapStateToProps = state => {
       favorites: state.favorites
     }
   }
+
 
 const mapDispatchToProps = dispatch => ({
     postFavorite: (dishId) => dispatch(postFavorite(dishId)),
@@ -66,6 +67,16 @@ function RenderDish(props) {
           return true;
       }
   })
+
+  const shareDish = (title, message, url) => {
+    Share.share({
+        title: title,
+        message: title + ': ' + message + ' ' + url,
+        url: url
+    },{
+        dialogTitle: 'Share ' + title
+    })
+}
     
         if (dish != null) {
             return(
@@ -97,6 +108,15 @@ function RenderDish(props) {
             color="#512DA8"
             onPress={() => props.toggleModal()}
           />
+          <Icon
+              raised
+              reverse
+              name='share'
+              type='font-awesome'
+              color='#51D2A8'
+              style={style.cardItem}
+              onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
+                            
         </View>
                 </Card>
                 </Animatable.View>
